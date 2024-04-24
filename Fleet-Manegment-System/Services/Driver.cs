41a132b6f73ca32659d0b9b82660864960597fb9
abood jalald <abood.jallad.2001@gmail.com>
2024-04-24 20:30:49 +0300

    define the main funcinality .

---
+++ Fleet-Manegment-System/Services/Driver.cs
@@ -0,0 +1,54 @@
+﻿using Fleet_Manegment_System.Modules;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fleet_Manegment_System.Services
+{
+    internal class Driver : IService
+    {
+
+        public void Add(GVAR gvar)
+        {
+            if (gvar.DicOfDic.ContainsKey("DriverInfo"))
+            {
+                var connection = DatabaseConnection.Instance.Connection;
+                var driverinfo = gvar.DicOfDic["DriverInfo"];
+                var sql = "INSERT INTO driver (drivername, phonenumber) VALUES (@drivername, @phonenumber)";
+                if (connection != null && connection.State == System.Data.ConnectionState.Open)
+                {
+                    using (var command = new NpgsqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", driverinfo["drivername"]);
+                        command.Parameters.AddWithValue("@number", driverinfo["phonenumber"]);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to execute insert; database connection is not available.");
+                }
+            }
+        }
+
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Fleet-Manegment-System/Services/IService.cs
@@ -0,0 +1,17 @@
+﻿using Fleet_Manegment_System.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fleet_Manegment_System.Services
+{
+    internal interface IService
+    {
+        void Add(GVAR gvar);
+        void Delete();
+        void Get();
+        void Update();
+    }
+}
